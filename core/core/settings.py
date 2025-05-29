f2470ef0346fb96e2a4d57c6a5320b01efb2beac
Kushal Yadav <ky1368177@gmail.com>
2025-05-29 17:04:12 +0530

    configured redis server with docker successfully, configured routing and consumers file.

--- core/core/settings.py
+++ core/core/settings.py
@@ -45,6 +45,16 @@ INSTALLED_APPS = [
 # asgi 
 ASGI_APPLICATION = "core.asgi.application"
 
+# channel layer 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
