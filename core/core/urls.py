64b21970e2f6313815b3db772409af6c2765c5ef
kushalyadav0 <ky1368177@gmail.com>
2025-05-31 21:58:17 +0530

    wrote connect method in consumers for real time connection establishment on asgi server.

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', include('core_app.urls'))
+    path('core/', include('core_app.urls')),
+    path('', include('core_app.urls')),
+    
+
 ]
--- core/core_app/consumers.py
+++ core/core_app/consumers.py
@@ -3,7 +3,16 @@ import json
 
 from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+    async def connect(self)
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+
+        # joining room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.room_name
+        )
+        
         self.accept()
 
     def disconnect(self, close_code):
