748cb876da0c1bcc55fcf6ae8b028b678717d79c
kushalyadav0 <ky1368177@gmail.com>
2025-06-05 07:53:09 +0530

    async functions

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import core_app.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -24,8 +24,8 @@ from core_app.routing import websocket_urlpatterns
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-        ),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                core_app.routing.websocket_urlpatterns))
     }
 )
--- core/core_app/consumers.py
+++ core/core_app/consumers.py
@@ -1,25 +1,36 @@
-from email import message
 import json
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
-from channels.generic.websocket import WebsocketConsumer
-class ChatConsumer(WebsocketConsumer):
-    async def connect(self)
+class ChatConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
 
         # joining room group
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.room_name
+            self.room_name,
         )
         
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.room_name)
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        self.send(text_data=json.dumps({'message':message}))
+        await self.channel_layer.group_send(self.room_group_name,{
+            'type':'chat_message',
+            'message':message
+        })
+
+        await self.send(text_data=json.dumps({'message':message}))
+
+    async def chat_message(self,event):
+        await self.send(text_data=json.dumps({
+            'messagge':event['message']
+        }))
