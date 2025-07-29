4fea52d6b275fe53aa362f0788139525f0ade006
kushalyadav0 <ky1368177@gmail.com>
2025-07-29 10:57:55 +0530

    Refactor chat and account templates; update chat URLs
    - Updated chat and account-related templates for improved UI/UX
    - Removed unused account_inactive.html template
    - Modified chat URLs for better routing

--- project/chat/urls.py
+++ project/chat/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lobby/', views.lobby), 
+    path('lobby/', views.lobby, name="lobby"), 
     path('chat/<str:room_name>/', views.chat,name='room')
 ]
