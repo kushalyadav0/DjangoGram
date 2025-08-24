5acdaf24d87a4442a0de98e869cc79a14b9da361
kushalyadav0 <ky1368177@gmail.com>
2025-08-24 16:40:49 +0530

    changes in headings to be more dynamic according to the room name and users

--- tests/main.js
+++ tests/main.js
@@ -4,6 +4,6 @@ const camera = async (constraints)=> {
     return await navigator.mediaDevices.getUserMedia(constraints);
 }
 window.onload = ()=> {
-    const stream = openMediaDevices({'video':true,'audio':true});
+    const stream = camera({'video':true,'audio':true});
     console.log('Got MediaStream:', stream);
 }
